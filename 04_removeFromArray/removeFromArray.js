ebff87e630c6281f324f5affb48935b2debbb060
thisisshiki <thisisshiki@outlook.com>
2023-12-01 15:57:53 +1100

    modified:   01_helloWorld/helloWorld.js 	modified:   02_repeatString/repeatString.js 	modified:   02_repeatString/repeatString.spec.js 	modified:   03_reverseString/reverseString.js 	modified:   03_reverseString/reverseString.spec.js 	modified:   04_removeFromArray/removeFromArray.js 	modified:   04_removeFromArray/removeFromArray.spec.js 	modified:   05_sumAll/sumAll.js 	modified:   05_sumAll/sumAll.spec.js 	modified:   06_leapYears/leapYears.js 	modified:   06_leapYears/leapYears.spec.js 	modified:   07_tempConversion/tempConversion.js 	modified:   07_tempConversion/tempConversion.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rm) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!rm.includes(arr[i])) {
+        newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
